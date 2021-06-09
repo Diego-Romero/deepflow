93952e0a7eb4ee1255b56dcb2e1ed86ec0c254f8
diego-romero <dev.diego.romero@gmail.com>
2021-06-08 19:08:36 -0600

    added google and github auth

--- components/FirebaseAuth.js
+++ components/FirebaseAuth.js
@@ -1,24 +1,29 @@
 /* globals window */
-import React, { useEffect, useState } from 'react'
-import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth'
-import firebase from 'firebase/app'
-import 'firebase/auth'
+import React, { useEffect, useState } from "react";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+import firebase from "firebase/app";
+import "firebase/auth";
 
 // Note that next-firebase-auth inits Firebase for us,
 // so we don't need to.
 
 const firebaseAuthConfig = {
-  signInFlow: 'popup',
+  // signInFlow: "popup",
   // Auth providers
   // https://github.com/firebase/firebaseui-web#configure-oauth-providers
   signInOptions: [
     {
-      provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID,
       requireDisplayName: false,
     },
+    // {
+    //   provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    //   requireDisplayName: false,
+    // },
+    firebase.auth.GithubAuthProvider.PROVIDER_ID,
   ],
-  signInSuccessUrl: '/',
-  credentialHelper: 'none',
+  signInSuccessUrl: "/",
+  credentialHelper: "none",
   callbacks: {
     // https://github.com/firebase/firebaseui-web#signinsuccesswithauthresultauthresult-redirecturl
     signInSuccessWithAuthResult: () =>
@@ -26,17 +31,17 @@ const firebaseAuthConfig = {
       // auth state in withAuthComponent.js.
       false,
   },
-}
+};
 
 const FirebaseAuth = () => {
   // Do not SSR FirebaseUI, because it is not supported.
   // https://github.com/firebase/firebaseui-web/issues/213
-  const [renderAuth, setRenderAuth] = useState(false)
+  const [renderAuth, setRenderAuth] = useState(false);
   useEffect(() => {
-    if (typeof window !== 'undefined') {
-      setRenderAuth(true)
+    if (typeof window !== "undefined") {
+      setRenderAuth(true);
     }
-  }, [])
+  }, []);
   return (
     <div>
       {renderAuth ? (
@@ -46,7 +51,7 @@ const FirebaseAuth = () => {
         />
       ) : null}
     </div>
-  )
-}
+  );
+};
 
-export default FirebaseAuth
+export default FirebaseAuth;
